efd948e0448c6f70c75e8fb934296aaf6bf186fe
David Durika <durika.d@gmail.com>
2015-02-04 22:22:30 +0100

    clean up

--- client/app.js
+++ client/app.js
@@ -1,9 +1,3 @@
-Meteor.startup(function(){
-  $('html').addClass('fluid top-full sticky-top sidebar sidebar-full sticky-sidebar');
-});
-
-
-
 Template.registerHelper('join', function(array) {
   return array.join(', ');
 });
--- server/config.js
+++ server/config.js
@@ -1,11 +1,1 @@
-Meteor.startup(function () {
-  //Users.update({_id:'7NJaFBgpXNqCScug8'}, {$unset: {roles: ""}});
-  //Users.update({_id:'fLvTks53YDdyFYMug'}, {$unset: {roles: ""}});
-  //Roles.setUserRoles('7NJaFBgpXNqCScug8', ["admin"]);
-});
 
-
-
-EditableText.userCanEdit = function(doc,Collection) {
-    return true;
-}
