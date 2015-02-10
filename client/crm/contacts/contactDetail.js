8a02b7ca252e4fe5866c88c4f351920e1c6679a4
David Durika <durika.d@gmail.com>
2015-02-10 19:45:00 +0100

    Contacts from: dynamically show/hide fields by type

--- client/crm/contacts/contactDetail.js
+++ client/crm/contacts/contactDetail.js
@@ -1,16 +1,24 @@
-Template[getTemplate('contactDetail')].events({
-  'keyup #company-id': function(event) {
-	var value = event.currentTarget.value;
- 	var search = {fulltext: value};
-	Meteor.call('findCompanyData', search, function(error, result) {
-	});
-  },
-  'change #type': function(event) {
-		console.log(event.target.value);
-	}
-});
-
-Template.contactDetail.rendered = function () {
-	$('select').material_select();
+Template[getTemplate('contactDetail')].rendered = function () {
+  $('select').material_select();
 };
 
+Template[getTemplate('contactDetail')].helpers({
+  typeIs: function(type, ifTrue, ifFalse) {
+    ifTrue = ifTrue || true;
+    ifFalse = ifFalse || false;
+    return this.type == type ? ifTrue : ifFalse;
+  }
+});
+
+Template[getTemplate('contactDetail')].events({
+  'keyup #company-id': function (event) {
+    var value = event.currentTarget.value;
+    var search = {fulltext: value};
+    Meteor.call('findCompanyData', search, function (error, result) {
+    });
+  },
+  'click .toggle-type': function (event, instance) {
+    var type = event.currentTarget.dataset.type;
+    Contacts.update(this._id, {$set: {type: type}});
+  }
+});
