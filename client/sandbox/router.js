ac5f02ee20fa124efc410107e2257d3987e9f35c
chren <peter.privalinec@gmail.com>
2015-02-28 18:25:51 +0100

    added contactListBox_sandbox component

--- client/sandbox/router.js
+++ client/sandbox/router.js
@@ -1,7 +1,7 @@
-Router.route('/sandbox/contactDetail', {
-  layoutTemplate: 'sandboxLayout',
-  name: 'contactDetail_sandbox',
-  template: 'contactDetail_sandbox',
+Router.route('/sandbox/contactListBox', {
+  layoutTemplate: 'contactListBox_sandbox',
+  name: 'contactListBox_sandbox',
+  template: 'contactListBox_sandbox',
   action: function () {
     this.render();
   }
